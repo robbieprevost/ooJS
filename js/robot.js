91d7ff56fd8c822749256c79b631417fa8f7c855
robbieprevost <robbieprevost@gmail.com>
2015-04-20 22:19:41 -0700

    exit maze functions
    added functions to the robot maze objects to exit the maze.

--- js/robot.js
+++ js/robot.js
@@ -43,3 +43,53 @@ Robot.prototype.turnLeft = function(){
     this.orientation = lefts[this.orientation];
     return true;
 };
+
+Robot.prototype.moveForward = function(){
+
+    if(!this.canMoveForward()) {
+        return false;
+    }
+        switch (this.orientation) {
+
+            case "north":
+                this.y += 1;
+                break;
+            case "east":
+                this.x += 1;
+                break;
+            case "south":
+                this.y -= 1;
+                break;
+            case "west":
+                this.x -= 1;
+                break;
+        }
+    return true;
+};
+
+Robot.prototype.canMoveForward = function(){
+    if(!this.maze){
+        return false;
+    }
+    return this.maze.canMove(this.x, this.y, this.orientation);
+};
+
+Robot.prototype.exitMaze = function(steps){
+    if(this.maze){
+        while(steps != 0) {
+            steps -= 1;
+            if (this.canMoveForward()) {
+                this.moveForward();
+                this.turnLeft();
+            } else {
+                this.turnRight();
+            }
+            if(this.x == this.maze.endX && this.x == this.maze.endY){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+};
